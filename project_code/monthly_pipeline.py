84d3d6ee98428d1c3ad00582c450a5cbc38af97b
Julia Ostheimer <42974945+JOPloume@users.noreply.github.com>
2026-01-28 20:44:10 +0100

    Add scraping of CodeFor data in monthly scraping pipeline

--- project_code/monthly_pipeline.py
+++ project_code/monthly_pipeline.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from Webscraping.Citylab_Berlin.scrape_projects_from_citylab_website import scrape_citylab_berlin
+from Webscraping.CodeFor.scrape_projects_from_codefor_website import scrape_codefor
 from Webscraping.get_ai_data import enrich_projects_data_with_ai
 from MarkdownConverter.data.csv.Combined_InsertDict import combine_projects_data
 from MarkdownConverter.OrganizationLinkFinder.organization_link_finder import find_correct_organization_links
@@ -12,13 +13,22 @@ citylab_berlin_projects = scrape_citylab_berlin(
 		save_to_csv=True
 	)
 
-# # Actually, with the further project setup it is better to write out the data as CSV file instead
 citylab_berlin_projects_enriched = enrich_projects_data_with_ai(
     citylab_berlin_projects,
     type_of_data="Citylab_Berlin"
     )
 
-# TODO: CodeFor Scraping
+# Scrape Code For Germany Projects
+codefor_projects = scrape_codefor(
+		url="https://codefor.de/projekte/alle/",
+		save_to_csv=True
+	)
+
+codefor_projects_enriched = enrich_projects_data_with_ai(
+    codefor_projects,
+    type_of_data="CodeFor"
+    )
+
 # TODO: Civic Coding Scraping
 
 
@@ -27,7 +37,8 @@ combine_projects_data(
         "project_code/Webscraping/Correlaid-Projektdatenbank/2026-01-19_Correlaid-Projekte-via-API_enriched.csv",
         "project_code/Webscraping/PublicInterestAI/PublicInterestAI_Projekte_enriched.csv",
         "project_code/Webscraping/Erfolgsgeschichten/Liste der Projekte Datenerfolgsgeschichten.csv",
-        citylab_berlin_projects_enriched
+        citylab_berlin_projects_enriched,
+        codefor_projects_enriched
     ]  
 )
 
